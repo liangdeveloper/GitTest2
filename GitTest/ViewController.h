a4b786df55424fb35a53879fd25c1dd5299b05ba
aiwalle <wallelj0909@gmail.com>
2021-01-13 16:54:56 +0800

    first init

---
+++ GitTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by liangjian on 2021/1/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by liangjian on 2021/1/13.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
