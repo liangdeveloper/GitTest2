54cd31f3c68db35c279dacf48dbc8425a104a1c8
liangdeveloper <61785636@qq.com>
2021-01-21 10:19:25 +0800

    添加了car2

---
+++ GitTest/LJCarTwo.h
@@ -0,0 +1,16 @@
+//
+//  LJCarTwo.h
+//  GitTest
+//
+//  Created by devLiang on 2021/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LJCarTwo : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitTest/LJCarTwo.m
@@ -0,0 +1,12 @@
+//
+//  LJCarTwo.m
+//  GitTest
+//
+//  Created by devLiang on 2021/1/21.
+//
+
+#import "LJCarTwo.h"
+
+@implementation LJCarTwo
+
+@end
