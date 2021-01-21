385338a730e7c6ca50d5dbf0bb70d67fe1d1bb1c
liangdeveloper <61785636@qq.com>
2021-01-21 14:08:41 +0800

    add car5

---
+++ GitTest/LJCarFive.h
@@ -0,0 +1,16 @@
+//
+//  LJCarFive.h
+//  GitTest
+//
+//  Created by devLiang on 2021/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LJCarFive : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitTest/LJCarFive.m
@@ -0,0 +1,12 @@
+//
+//  LJCarFive.m
+//  GitTest
+//
+//  Created by devLiang on 2021/1/21.
+//
+
+#import "LJCarFive.h"
+
+@implementation LJCarFive
+
+@end
