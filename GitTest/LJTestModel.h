2c812953251295e8c6f4f2742a244d10801b35bf
liangdeveloper <61785636@qq.com>
2021-01-29 15:53:45 +0800

    hello world

---
+++ GitTest/LJTestModel.h
@@ -0,0 +1,16 @@
+//
+//  LJTestModel.h
+//  GitTest
+//
+//  Created by devLiang on 2021/1/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LJTestModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitTest/LJTestModel.m
@@ -0,0 +1,12 @@
+//
+//  LJTestModel.m
+//  GitTest
+//
+//  Created by devLiang on 2021/1/29.
+//
+
+#import "LJTestModel.h"
+
+@implementation LJTestModel
+
+@end
